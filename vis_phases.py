3e36ecb742bf1cb369a81959d17bab4f62155edd
Daniel Varga <daniel.varga@prezi.com>
2021-12-12 10:07:08 +0100

    vis_phases fixed

--- vis_phases.py
+++ vis_phases.py
@@ -1,10 +1,63 @@
-# input: canonized_mubs
+# input: three lines of canonized_mubs, like
+# cat canonized_mubs | grep mub_100.npy | python analyze_canonized_mubs.py
 
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
 # filename normalized/mub_100.npy i 1 D_l [  -0.      -26.7955 -173.147   -38.4863 -168.1707 -150.0384] P_l [0 5 2 3 1 4] x 55.11879687165239 y 0.0005514574849221608 P_r [4 1 2 3 0 5] D_r [0. 0. 0. 0. 0. 0.] distance 4.003481295679986e-06
+
+
+TP = 2 * np.pi
+TIP = 2j * np.pi
+W = np.exp(TIP / 3)
+
+
+def degrees_to_phases(d):
+    return np.exp(1j * np.pi / 180 * d)
+
+
+def perm_to_pm(p):
+    return np.eye(6)[p, :]
+
+
+# TODO build a lib, do not copypaste this function everywhere.
+# x and y are phases. this is exactly formula (3) in https://arxiv.org/pdf/0902.0882.pdf
+# not sure about numerical precision when doing (sixth ** 5) instead of -W.
+def canonical_fourier(x, y):
+    ws = np.ones((6, 6), dtype=np.complex128)
+    ws[1:6:3, 1:6:2] *= x
+    ws[2:6:3, 1:6:2] *= y
+    sixth = - np.conjugate(W)
+    for i in range(1, 6):
+        for j in range(1, 6):
+            ws[i, j] *= sixth ** ((i * j) % 6)
+    return ws
+
+
+def factors_to_basis(factors):
+    left_phases, left_pm, x, y, right_pm, right_phases = factors
+    return np.diag(left_phases) @ left_pm @ canonical_fourier(x, y) @ right_pm @ np.diag(right_phases)
+
+
+def serialized_to_factors(serialized_basis):
+    left_degrees, left_permutation, x_degrees, y_degrees, right_permutation, right_degrees = serialized_basis
+    x = degrees_to_phases(x_degrees)
+    y = degrees_to_phases(y_degrees)
+    left_phases = degrees_to_phases(left_degrees)
+    right_phases = degrees_to_phases(right_degrees)
+
+    # beware of the T here!!! left and right permutation matrices
+    # are represented differently as permutations.
+    left_pm = perm_to_pm(left_permutation).T
+    right_pm = perm_to_pm(right_permutation)
+
+    factors = (left_phases, left_pm, x, y, right_pm, right_phases)
+    basis = factors_to_basis(factors)
+    assert np.allclose(np.conjugate(basis.T) @ basis, 6 * np.eye(6)), "does not seem to be a basis: " + str(np.conjugate(basis.T) @ basis)
+    assert np.allclose(np.abs(basis), 1), "does not seem to be Hadamard: " + str(np.abs(basis))
+    return factors
+
 
 mubs = {}
 for l in sys.stdin:
@@ -13,52 +66,36 @@ for l in sys.stdin:
     assert len(a) == 38
     filename = a[1]
     basis = int(a[3])
-    degrees = np.array(list(map(float, a[5:11])))
+    left_degrees = np.array(list(map(float, a[5: 5 + 6])))
+    right_degrees = np.array(list(map(float, a[30: 30 + 6])))
+    left_permutation = np.array(list(map(int, a[12: 12 + 6])))
+    right_permutation = np.array(list(map(int, a[23: 23 + 6])))
     assert a[18] == "x"
-    x = float(a[19])
-    y = float(a[21])
+    x_degrees = float(a[19])
+    y_degrees = float(a[21])
     if filename not in mubs:
         mubs[filename] = []
-    mubs[filename].append((basis, degrees, x, y))
+    factors = serialized_to_factors((left_degrees, left_permutation, x_degrees, y_degrees, right_permutation, right_degrees))
+    mubs[filename].append(factors)
 
-cool = []
 
+phase_collection = []
 for filename, mub in mubs.items():
-    if len(mub) != 3:
-        print(filename, "partially reconstructed, dropped")
-        continue
-    cool_index = None
-    boring_count = 0
-    permutation = None
     for i in range(3):
-        basis, degrees, x, y = mub[i]
-        if np.abs(degrees).sum() < 1e-3:
-            # print("that's the weird one with zero phases, dropped for now")
-            cool_index = None
-            break
-        if np.allclose(np.sort(degrees), np.array([-120, -120, 0, 0, 120, 120])):
-            boring_count += 1
-            permutation = np.argsort(degrees)
-        else:
-            cool_index = i
-    if cool_index is None:
-        continue
-    assert boring_count == 2, mub
-    cool_degrees = mub[cool_index][1]
-    cool_degrees = cool_degrees[permutation]
-    cool.append(cool_degrees)
-    print(cool_degrees[:, None] - cool_degrees[None, :])
+        left_phases, left_pm, x, y, right_pm, right_phases = mub[i]
+        # left_phases = np.concatenate([left_phases, - left_phases])
+        phase_collection.append(left_phases * (2+i) / 4)
 
 
+cool = phase_collection
 n = int(np.ceil(np.sqrt(len(cool))))
 while len(cool) < n ** 2:
-    cool.append(np.zeros_like(cool_degrees))
+    cool.append(np.zeros_like(cool[-1]))
     print("adding dummy data")
 
-cool = np.array(cool).reshape((n, n, 6))
+cool = np.array(cool).reshape((n, n, -1))
+print(cool.shape)
 
-
-cool_phases = np.exp(1j * np.pi / 180 * cool)
 
 x = np.linspace(0, 4 * n, n)
 y = np.linspace(0, 4 * n, n)
@@ -66,5 +103,5 @@ xx, yy = np.meshgrid(x, y)
 xx = xx[:, :, None]
 yy = yy[:, :, None]
 
-plt.scatter(xx + np.real(cool_phases), yy + np.imag(cool_phases), s=4)
+plt.scatter(xx + np.real(cool), yy + np.imag(cool), s=4)
 plt.show()
