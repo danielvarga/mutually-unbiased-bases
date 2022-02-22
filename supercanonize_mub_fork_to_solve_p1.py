b623cf5cc4ca191ce2b236ca26d7abced5019120
Daniel Varga <daniel.varga@prezi.com>
2022-02-22 14:48:01 +0100

    before switching from closure to value_dict

--- supercanonize_mub_fork_to_solve_p1.py
+++ supercanonize_mub_fork_to_solve_p1.py
@@ -11,6 +11,8 @@ import matplotlib.pyplot as plt
 TP = 2 * np.pi
 TIP = 2j * np.pi
 W = np.exp(TIP / 3)
+WW = np.exp(TIP / 12)
+BOARD = [(i, j) for i in range(6) for j in range(6)]
 
 
 def degrees_to_phases(d):
@@ -439,49 +441,86 @@ alpha02 = 0.38501828415482425+0.9229089450571356j
 delta02 = 0.923033846322939-0.3847187525222561j
 
 
-# only works reliably for mub_10024 P01, P02.
-def find_delta(b0, b1):
+# between -30 and 30 actually, so that 60-divisible angles
+# stably land around 0.
+def rot_to_60(p_orig):
+    p = p_orig
+    while not(-np.pi / 6 <= np.angle(p) < np.pi / 6):
+        p *= -np.conjugate(W)
+    return p
+
+
+
+def identify_alpha_delta(b0, b1):
     prod = np.conjugate(b0.T) @ b1
+    msquared = np.abs(prod) ** 2
     normed = prod.copy()
     normed /= np.abs(normed)
-    alpha_hm = normed.sum() / np.abs(normed.sum())
-    print("alpha_hm", angler(alpha_hm))
-    alpha = normed[0, 0]
-    beta = normed[0, 1]
-    delta = beta / alpha
+
+    magnitude_masks = [np.isclose(msquared, numerical_magic_constants[i], atol=1e-4).astype(int) for i in range(3)]
+    magnitude_masks = np.array(magnitude_masks)
+
+    A_mask = magnitude_masks[0]
+    collection = []
+    for (i, j) in BOARD:
+        if A_mask[i, j]:
+            rot = normed[i, j]
+            collection.append(rot)
+    collection = np.array(collection)
+
+    coll_60 = []
+    epsilon = 0.01
+    for rot in collection:
+        while not(epsilon <= np.angle(rot) < np.pi / 3 + epsilon):
+            rot *= np.exp(1j * np.pi / 3)
+        coll_60.append(rot)
+    coll_60 = np.array(coll_60)
+    # -> coll_60 is supposed to have only 3 distinct values up to numerical precision.
+    #    12 alpha, 6 alpha delta, 6 alpha conj(delta).
+    pairs = coll_60[None, :] / coll_60[:, None]
+    neighbors = np.isclose(pairs, 1, atol=1e-4)
+    neighbor_counts = neighbors.sum(axis=1)
+    # we are interested in the ones that have exactly 12 neighbors:
+    alpha_circle = collection[neighbor_counts == 12]
+    # plt.scatter(collection.real, collection.imag) plt.show()
+    assert len(alpha_circle) == 12
+
+    # any element of alpha_circle will do as alpha, it's fully symmetric
+    # we break tie by choosing the one closest to 0 degrees.
+    dist_to_0 = np.abs(np.angle(alpha_circle))
+    i = np.argmin(dist_to_0)
+    alpha = alpha_circle[i]
+    collection /= alpha
+    alpha_circle = collection[neighbor_counts == 12]
+
+    # more exactly delta union conj(delta) circle:
+    delta_circle = collection[neighbor_counts == 6]
+    assert len(delta_circle) == 12
+    # after the previous division by alpha,
+    # any element of the delta_circle will do as delta.
+    # we break the tie by choosing the one closest to 0,
+    # that's supposed to be either ~7.3737 degrees or ~60-7.3737 degrees.
+    # we then mirror it to ~7.3737.
+    dist_to_0 = np.abs(np.angle(delta_circle))
+    i = np.argmin(dist_to_0)
+    delta = delta_circle[i]
+    alpha = rot_to_60(alpha)
+    delta = rot_to_60(delta)
+    if angler(delta) < 0:
+        delta = np.conjugate(delta)
+        if angler(delta) > 15:
+            delta = WW * np.conjugate(delta)
+            assert angler(delta) < 15
     print("alpha", angler(alpha), "delta", angler(delta))
-    assert np.isclose(normed[1, 0], alpha / delta), "this is a restriction currently, please use mub_10024.py or generalize"
     return alpha, delta
 
 
-# at this point we hardwire mub_10024.py, sorry.
-# its distinct characteristic is that the tripartition
-# looks like this:
-# .U..V.
-# U..V..
-# ..V..U
-# .V..U.
-# V..U..
-# ..U..V
-# (U=0.35, V=0.38, .=0.42)
-# thanks to this, the top left corner of its normalized form obeys these rules:
-# ab.... (alpha, beta, gamma really)
-# c.....
-# ......
-# beta/alpha = alpha/gamma
-# delta := beta/alpha
-# other than that, we are general, so it would not be hard to cover all qmubs.
 # BEWARE: the result is yet to be multiplied by 6 * alphasym.
 def reconstruct_product(b0, b1, alpha, delta, alphasym, deltasym):
     prod = np.conjugate(b0.T) @ b1
     normed = prod.copy()
     normed /= np.abs(normed)
-    import matplotlib.pyplot as plt
-    vecs = (normed / normed[0, 0]).flatten()
-    print(np.sort(angler(vecs)))
-    # plt.scatter(vecs.real, vecs.imag) ; plt.show()
 
-    WW = np.exp(TIP / 12)
     roots = WW ** np.arange(12)
     candidates = np.array([1, delta, 1 / delta])
     all_candidates = alpha * candidates[:, None] * roots[None, :]
@@ -501,7 +540,7 @@ def reconstruct_product(b0, b1, alpha, delta, alphasym, deltasym):
             rotation = np.argmax(onehot.sum(axis=0))
             assert onehot[element_index, rotation] == 1
             normed_prod_sym[i, j] *= candidates_sym[element_index] * roots_sym[rotation]
-    assert np.allclose(sym_to_num(alphasym * normed_prod_sym), normed)
+    # assert np.allclose(sym_to_num(alphasym * normed_prod_sym), normed)
 
     msquared = np.abs(prod) ** 2
     masks = [np.isclose(msquared, numerical_magic_constants[i]).astype(int) for i in range(3)]
@@ -511,10 +550,33 @@ def reconstruct_product(b0, b1, alpha, delta, alphasym, deltasym):
         for j in range(6):
             element_index = np.argmax(masks[:, i, j])
             magnitude_matrix_sym[i, j] *= magnitudes_sym[element_index]
-    assert np.allclose(sym_to_num(6 * magnitude_matrix_sym), np.abs(prod))
+    # assert np.allclose(sym_to_num(6 * magnitude_matrix_sym), np.abs(prod))
     final_result = matrix_multiply_elementwise(normed_prod_sym, magnitude_matrix_sym)
-    assert np.allclose(sym_to_num(6 * alphasym * final_result), prod)
+    # assert np.allclose(sym_to_num(6 * alphasym * final_result), prod)
     return final_result
+
+
+def reconstruct_product_full_service(b0, b1, indx):
+    alpha, delta = identify_alpha_delta(b0, b1)
+    alphasym, deltasym = sympy.symbols(f"alpha{indx}, delta")
+    p = reconstruct_product(b0, b1, alpha, delta, alphasym, deltasym)
+    return p
+
+
+def alpha_removal(mub):
+    alpha1, delta1 = identify_alpha_delta(mub[0], mub[1])
+    mub[1] /= alpha1
+    alpha2, delta2 = identify_alpha_delta(mub[0], mub[2])
+    mub[2] /= alpha2
+
+    alpha1, delta1 = identify_alpha_delta(mub[0], mub[1])
+    assert np.isclose(alpha1 ** 6, 1, atol=1e-3)
+    alpha2, delta2 = identify_alpha_delta(mub[0], mub[2])
+    assert np.isclose(alpha2 ** 6, 1, atol=1e-3)
+
+    alpha0, delta0 = identify_alpha_delta(mub[1], mub[2])
+    assert np.isclose(alpha0 ** 6, 1, atol=1e-3)
+    return mub
 
 
 def sym_to_num(formula):
@@ -548,12 +610,14 @@ def create_symbolic_mub(factorized_mub):
     return symbolic_bases
 
 
-alpha, delta = find_delta(mub[0], mub[1])
+mub = alpha_removal(mub)
+
+alpha, delta = identify_alpha_delta(mub[0], mub[1])
 prod01reconstructed_sym = reconstruct_product(mub[0], mub[1], alpha, delta, alpha01sym, delta01sym)
 print("B_1^\\dag B_2 = 6 \\alpha_2", sympy.latex(prod01reconstructed_sym))
 prod01reconstructed_sym *= 6 * alpha01sym # it immediately executes, so it's nicer before it
 
-alpha, delta = find_delta(mub[0], mub[2])
+alpha, delta = identify_alpha_delta(mub[0], mub[2])
 prod02reconstructed_sym =  reconstruct_product(mub[0], mub[2], alpha, delta, alpha02sym, delta02sym)
 print("B_1^\\dag B_3 = 6 \\alpha_3", sympy.latex(prod02reconstructed_sym))
 prod02reconstructed_sym *= 6 * alpha02sym
