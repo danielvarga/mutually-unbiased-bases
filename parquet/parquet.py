867946593b5d20d333e12879d3f24f23062eadb4
Daniel Varga <daniel.varga@prezi.com>
2022-06-24 00:47:42 +0200

    LP lower and upper bound

--- parquet/parquet.py
+++ parquet/parquet.py
@@ -1,7 +1,7 @@
 import numpy as np
 from pulp import *
 
-n = 4
+n = 6
 N = 8
 
 x = np.arange(-N+1, N, dtype=int)
@@ -17,6 +17,7 @@ b = - np.sort(- b, axis=-1)
 # keeping only the distinct ones:
 b = np.unique(b, axis=0)
 variables = set(tuple(var) for var in b)
+
 
 lp = LpProblem("parquet", LpMinimize)
 # for each element of variable, we create an LP variable bounded between 0 and 1:
@@ -45,7 +46,7 @@ for var in variables:
                 break
             parq_vars.append(vardict[var_i])
         if available:
-            lp += sum(parq_vars) == 1
+            lp += lpSum(parq_vars) == 1
 
 
 # for example, build(1, 1, -1, -1) is the LP var corresponding to the [1, 1, 0, ..., 0, -1, -1] vector.
@@ -54,14 +55,22 @@ def build(*v):
 
 # we'd like to know the minimum possible value of the following variables
 # or sum of variables:
-target = build(1, 1, -1, -1)
+target = build(1, 1, 1, -1, -1, -1)
 # target = build(n, -n)
-lp += target
 
 target1 = build(2, -1, -1)
 target2 = build(1, 1, -1, -1)
-# lp += target1 + target2
+target = target1 + target2
 
 
-lp.solve()
-print(target, ">=", lp.objective.value())
+def lower_upper_bound(lp, target):
+    lp += target
+    lp.solve()
+    minimum = lp.objective.value()
+    lp += - target
+    lp.solve()
+    maximum = - lp.objective.value()
+    return minimum, maximum
+
+minimum, maximum = lower_upper_bound(lp, target)
+print(minimum, "<=", target, "<=", maximum)
