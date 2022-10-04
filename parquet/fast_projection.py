a8000216f3d76aeb0b7441a71d9a5f80ae73665c
Daniel Varga <daniel.varga@prezi.com>
2022-10-05 01:05:28 +0200

    cleanup

--- parquet/fast_projection.py
+++ parquet/fast_projection.py
@@ -20,32 +20,38 @@ b_ordered = - np.sort(- b, axis=-1)
 # keeping only the distinct ones:
 b_unique, unique_inverse = np.unique(b_ordered, axis=0, return_inverse=True)
 # -> b_unique[unique_inverse] == b_ordered
-variables = set(tuple(var) for var in b_unique)
 
 print(b_unique.shape)
+
+
+# assumes global variables b, b_unique, unique_inverse
+def evaluate(H):
+    # gridpoint x row x column
+    allpairs = H[None, :, :, ] ** b[:, :, None]
+    values = np.linalg.norm(allpairs.sum(axis=1), axis=1) ** 2
+    # -> indexed with gridpoints
+    print(values.shape)
+
+    values /= n ** 3
+
+    print("G min/avg/max", values.min(), values.mean(), values.max())
+
+    sums = np.zeros(len(b_unique))
+    counts = np.zeros(len(b_unique))
+    for i in range(len(values)):
+        which_in_bu = unique_inverse[i]
+        sums[which_in_bu] += values[i]
+        counts[which_in_bu] += 1
+
+    averages = sums / counts
+    print("Gbar min/avg/max", averages.min(), averages.mean(), averages.max())
+
+    # -> averages[i] tells the Gbar value for gridpoint b_unique[i]
+    return b_unique, averages
+
 
 MUB = np.load("../data/triplet_mub_57638.npy")
 H = MUB[0] * n ** 0.5
 assert np.allclose(np.abs(H), 1)
 
-# gridpoint x row x column
-allpairs = H[None, :, :, ] ** b[:, :, None]
-values = np.linalg.norm(allpairs.sum(axis=1), axis=1) ** 2
-# -> indexed with gridpoints
-print(values.shape)
-
-values /= n ** 3
-
-print("G min/avg/max", values.min(), values.mean(), values.max())
-
-sums = np.zeros(len(b_unique))
-counts = np.zeros(len(b_unique))
-for i in range(len(values)):
-    which_in_bu = unique_inverse[i]
-    sums[which_in_bu] += values[i]
-    counts[which_in_bu] += 1
-
-averages = sums / counts
-print("Gbar min/avg/max", averages.min(), averages.mean(), averages.max())
-
-# -> averages[i] tells the Gbar value for gridpoint b_unique[i]
+b_unique, averages = evaluate(H)
