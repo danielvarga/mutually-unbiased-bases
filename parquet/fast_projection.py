cee78be737100eb595a114011962a914c005740e
Daniel Varga <daniel.varga@prezi.com>
2022-10-05 08:51:47 +0200

    vectorize over batches

--- parquet/fast_projection.py
+++ parquet/fast_projection.py
@@ -2,7 +2,8 @@ import numpy as np
 
 
 n = 6
-N = 8
+N = 4
+
 
 x = np.arange(-N+1, N, dtype=int)
 xs = [x] * n
@@ -25,25 +26,34 @@ print(b_unique.shape)
 
 
 # assumes global variables b, b_unique, unique_inverse
-def evaluate(H):
-    # gridpoint x row x column
-    allpairs = H[None, :, :, ] ** b[:, :, None]
+# b is (num_total_gridpoints, n), Hs is (batchsize, n, n)
+def evaluate(Hs):
+    num_total_gridpoints = len(b)
+    num_unique_gridpoints = len(b_unique)
+    assert b.shape == (num_total_gridpoints, n)
+    batchsize = len(Hs)
+    Hs = np.transpose(Hs, (1, 2, 0))
+    assert Hs.shape == (n, n, batchsize)
+
+    allpairs = Hs[None, :, :, :] ** b[:, :, None, None]
+    assert allpairs.shape == (num_total_gridpoints, n, n, batchsize) # there's our memory bottleneck
     values = np.linalg.norm(allpairs.sum(axis=1), axis=1) ** 2
-    # -> indexed with gridpoints
-    print(values.shape)
+    assert values.shape == (num_total_gridpoints, batchsize)
 
     values /= n ** 3
 
     print("G min/avg/max", values.min(), values.mean(), values.max())
 
-    sums = np.zeros(len(b_unique))
-    counts = np.zeros(len(b_unique))
+    output_shape = (num_unique_gridpoints, batchsize)
+    sums = np.zeros(output_shape)
+    counts = np.zeros(output_shape)
     for i in range(len(values)):
         which_in_bu = unique_inverse[i]
-        sums[which_in_bu] += values[i]
-        counts[which_in_bu] += 1
+        sums[which_in_bu, :] += values[i, :]
+        counts[which_in_bu, :] += 1
 
     averages = sums / counts
+    assert averages.shape == output_shape
     print("Gbar min/avg/max", averages.min(), averages.mean(), averages.max())
 
     # -> averages[i] tells the Gbar value for gridpoint b_unique[i]
@@ -52,6 +62,12 @@ def evaluate(H):
 
 MUB = np.load("../data/triplet_mub_57638.npy")
 H = MUB[0] * n ** 0.5
-assert np.allclose(np.abs(H), 1)
+assert np.allclose(np.abs(H[0]), 1)
 
-b_unique, averages = evaluate(H)
+batchsize = 1000
+Hs = np.array([H] * batchsize)
+
+import time
+start = time.perf_counter()
+b_unique, averages = evaluate(Hs)
+print(time.perf_counter() - start, f"seconds for {batchsize} {n}x{n} matrices, gridsize {2 * N - 1}^{n}, {len(b_unique)} unique gridpoints")
