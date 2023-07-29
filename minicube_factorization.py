53732fb05c08627cfefcb338ae79f8d9a7a01aad
Daniel Varga <daniel.varga@prezi.com>
2023-07-29 18:30:18 +0200

    minicubes that can be rearranged into Szollosi normal form matrices.

--- minicube_factorization.py
+++ minicube_factorization.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 import itertools
 
@@ -42,34 +43,136 @@ def proper_2x2x2_blocks(c):
     return True
 
 
+filename, = sys.argv[1:] # "canonized_cubes/canonized_cube_00018.npy"
+c = np.load(filename)
 
-c = np.load("canonized_cubes/canonized_cube_00018.npy")
-
+'''
 # TODO something here
-yperm = [0, 3, 1, 5, 2, 4]
-zperm = [0, 5, 1, 4, 2, 3]
+yperm = [0, 5, 1, 4, 2, 3]
+zperm = [0, 4, 1, 3, 2, 5] # [0, 1, 2, 5, 4, 3]
 
 c = c[:, yperm, :]
 c = c[:, :, zperm]
 
+print(f"applying yperm {yperm} zperm {zperm}. managed to make it out of proper 2x2x2 blocks?:", proper_2x2x2_blocks(c))
+'''
 
-
-ps = list(permutations(range(6)))
-c_orig = c.copy()
-found = False
-for yperm in ps:
-    for zperm in ps:
-        c_permuted = c_orig[:, yperm, :]
-        c_permuted = c_permuted[:, :, zperm]
-        if proper_2x2x2_blocks(c_permuted):
-            found = True
+# not just any canonizer, but the lexicographically smallest one
+def find_canonizer(c):
+    ps = list(itertools.permutations(range(6)))
+    c_orig = c.copy()
+    found = False
+    for yperm in ps:
+        for zperm in ps:
+            c_permuted = c_orig[:, yperm, :]
+            c_permuted = c_permuted[:, :, zperm]
+            if proper_2x2x2_blocks(c_permuted):
+                found = True
+                break
+        if found:
             break
     if found:
-        break
-if found:
-    print("FOUND IT!")
-    print("yperm", yperm, "zperm", zperm)
-    cv = visualize_clusters(c, group_conjugates=False)
-    print(cv)
+        return yperm, zperm
+    else:
+        return None
+
+
+def find_all_canonizers(c):
+    ps = list(itertools.permutations(range(6)))
+    c_orig = c.copy()
+    found = []
+    for yperm in ps:
+        for zperm in ps:
+            c_permuted = c_orig[:, yperm, :]
+            c_permuted = c_permuted[:, :, zperm]
+            if proper_2x2x2_blocks(c_permuted):
+                found.append((yperm, zperm))
+    return sorted(found)
+
+
+def code_to_perm(code):
+    p = np.arange(6, dtype=int)
+    if code % 2 == 1:
+        p = p[[1, 0, 2, 3, 4, 5]]
+    if (code // 2) % 2 == 1:
+        p = p[[0, 1, 3, 2, 4, 5]]
+    if (code // 4) % 2 == 1:
+        p = p[[0, 1, 2, 3, 5, 4]]
+    return p
+
+
+def deinterlace(c):
+    c = c[:, [0, 2, 4, 1, 3, 5], :]
+    c = c[:, :, [0, 2, 4, 1, 3, 5]]
+    return c
+
+
+def mirror(c):
+    c = c[:, [0, 1, 2, 5, 4, 3], :]
+    c = c[:, :, [0, 1, 2, 5, 4, 3]]
+    return c
+
+
+def deinterlace_and_mirror(c):
+    return mirror(deinterlace(c))
+
+
+# a tricanonizer permutes the second two axes of a canonized cube so that it's tricanonized while remaining canonized.
+# canonized here means:
+#    - first axis is Szollosi,
+#    - first axis has 3x2 block structure (aka dual pairs)
+#    - whole cube has a 3x3x3 block structure of 2x2x2 blocks,
+#             where each 2x2x2 is a minicube aka a cube with all slices being Hadamard.
+# tricanonized means:
+#    after applying the fixed deinterlacing permutations [:, [0, 2, 4, 1, 3, 5], :] and [:, :, [0, 2, 4, 1, 3, 5]],
+#    and also applying the fixed mirroring permutations [:, [0, 1, 2, 5, 4, 3], :] and [:, :, [0, 1, 2, 5, 4, 3]],
+#    the Szollosi slices are in the standard block-circulant (although bistochastic) form.
+# out of the (2^3)^2 potential tricanonizers (perm_y, perm_z), exactly 2 is an actual tricanonizer,
+# and you get one from the other by transposing all three pairs, as in
+# [0 1 2 3 4 5] [0 1 2 3 4 5]
+# [1 0 3 2 5 4] [1 0 3 2 5 4]
+# UPDATE: it seems like tricanonization is a no-op, [0 1 2 3 4 5] [0 1 2 3 4 5] is always a tricanonizer, because earlier the canonizer 
+# has found the lexicographically smallest canonization starting from a block-circulant MUB-cube.
+def find_tricanonizer(c_orig):
+    for code_y in range(8):
+        perm_y = code_to_perm(code_y)
+        for code_z in range(8):
+            perm_z = code_to_perm(code_z)
+            c = c_orig.copy()
+            c = c[:, perm_y, :]
+            c = c[:, :, perm_z]
+            c_triblock = deinterlace_and_mirror(c)
+            diag1 = np.diag(c_triblock[0, :3, :3])
+            diag2 = np.diag(c_triblock[0, 3:, 3:])
+            if np.allclose(diag1, diag1.mean()) and np.allclose(diag2, diag2.mean()):
+                print(perm_y, perm_z)
+                # return perm_y, perm_z
+    raise Exception("no tricanonizer found")
+
+
+'''
+found = find_all_canonizers(c)
+print("number of 2x2x2 canonizers", len(found)) ; exit()
+'''
+
+result = find_canonizer(c)
+if result is None:
+    print("filename", filename, "no canonizer found")
+    exit()
 else:
-    print("nope :(")
+    yperm, zperm = result
+    c = c[:, yperm, :]
+    c = c[:, :, zperm]
+    assert proper_2x2x2_blocks(c)
+
+    try:
+        yperm_tri, zperm_tri = find_tricanonizer(c)
+    except:
+        print("filename", filename, "no tricanonizer found")
+        exit()
+
+    c = c[:, yperm_tri, :]
+    c = c[:, :, zperm_tri]
+    assert proper_2x2x2_blocks(c)
+
+    cv = visualize_clusters(c, group_conjugates=False) ; print(cv)
